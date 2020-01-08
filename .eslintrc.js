08795caf2f734ea9fa911d9bbcb27de93e9b7601
Nagornykh Maria <manyaknag@gmail.com>
2020-01-08 21:55:25 +0300

    начало

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,26 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    es6: true
   },
-  'extends': [
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
     'plugin:vue/essential',
     '@vue/standard'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // override default options for rules from base configurations
+    'arrow-parens': ['error', 'always'],
+    'comma-dangle': ['error', 'only-multiline'],
+    // disable rules from base configurations
+    indent: ['off', 2],
+    'spaced-comment': 'off',
+    'prefer-const': 'off',
+    semi: ['error', 'always'],
   }
-}
+};
