2dc21114b7a851055324ed02b70d55c3555f2cdb
Nagornykh Maria <manyaknag@gmail.com>
2020-01-09 12:48:24 +0300

    настройка цсс модулей

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   css: {
-    requireModuleExtension: true
-  }
+    requireModuleExtension: true,
+    sourceMap: true,
+    loaderOptions: {
+      css: {
+        modules: {
+          localIdentName:
+            process.env.NODE_ENV === 'production'
+              ? '[name]__[local]--[hash:base64:5]'
+              : '[name]__[local]'
+        },
+        localsConvention: 'camelCaseOnly'
+      }
+    }
+  },
+  productionSourceMap: false
 };
