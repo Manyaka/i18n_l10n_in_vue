91e19c8d9dfe149a04dfc3cb5d9492d290007789
Nagornykh Maria <manyaknag@gmail.com>
2020-01-09 12:59:26 +0300

    верстка статьи

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -19,6 +19,7 @@ export default {
 <style>
 .SiteHeader {
   text-align: center;
+  margin-bottom: 2em;
 }
 </style>
 <style module>
--- src/components/SomeArticle.vue
+++ src/components/SomeArticle.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="SomeArticle">
-    <h2 class="title">{{ articleTitle }}</h2>
-    <p class="date">{{ articleDate }}</p>
-    <p class="body">{{ articleBody }}</p>
+    <h2 :class="$style.title">{{ articleTitle }}</h2>
+    <p :class="$style.date">{{ articleDate }}</p>
+    <p :class="$style.body">{{ articleBody }}</p>
   </div>
 </template>
 
@@ -12,22 +12,58 @@ export default {
   props: {
     articleTitle: {
       type: String,
-      default: 'articleTitle'
+      default: 'Заголовок статьи'
     },
     articleDate: {
       type: String,
-      default: 'articleDate'
+      default: '30.10.2020'
     },
     articleBody: {
       type: String,
-      default: 'articleBody'
+      default:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur deserunt ea eos excepturi ipsa natus nostrum rem sed soluta velit! Distinctio facere repudiandae sequi soluta sunt. Amet distinctio eum ex.'
     }
   }
 };
 </script>
 
-<style module>
+<style>
 .SomeArticle {
-  visibility: visible;
+  background-color: #eaa1ad26;
+  border: 1px solid #e6626229;
+  border-radius: 5px;
+  padding: 1em;
+}
+
+.SomeArticle:not(:last-child) {
+  margin-bottom: 1em;
+}
+
+.SomeArticle p {
+  margin-top: 0;
+}
+
+.SomeArticle p:last-child {
+  margin-bottom: 0;
+}
+</style>
+<style module>
+.title {
+  margin-top: 0;
+}
+
+.date {
+  color: grey;
+}
+
+.body {
+  font-size: 1em;
+  line-height: 1.3;
+}
+
+@media screen and (min-width: 768px) {
+  .body {
+    font-size: 1.2em;
+  }
 }
 </style>
