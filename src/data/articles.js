5f98dfdb3e60f1451706c4ed8685ed92615afc23
Nagornykh Maria <manyaknag@gmail.com>
2020-01-18 00:37:54 +0300

    увеличила количество статей

--- src/data/articles.js
+++ src/data/articles.js
@@ -28,5 +28,25 @@ export default [
       'Вы можете поместить файлы локализаций, совместимые с vue-i18n в каталог locales в корне вашего плагина. Они будут автоматически загружены в клиент при открытии проекта. Вы можете использовать $t для перевода строк в ваших компонентах и другие возможности vue-i18n. Также, любые строки используемые в UI API (такие как describeTask) будут также обрабатываться vue-i18n, поэтому вы сможете локализовать и их.',
     body_en:
       'Different standards may need different levels of granularity in the date and time, so this profile defines six levels. Standards that reference this profile should specify one or more of these granularities. If a given standard allows more than one granularity, it should specify the meaning of the dates and times with reduced precision, for example, the result of comparing two dates with different precisions.'
+  },
+  {
+    id: 4,
+    title: 'Заголовок статьи',
+    title_en: 'Article title',
+    date: '1997-07-16T19:20+01:00',
+    body:
+      'Вы можете поместить файлы локализаций, совместимые с vue-i18n в каталог locales в корне вашего плагина. Они будут автоматически загружены в клиент при открытии проекта. Вы можете использовать $t для перевода строк в ваших компонентах и другие возможности vue-i18n. Также, любые строки используемые в UI API (такие как describeTask) будут также обрабатываться vue-i18n, поэтому вы сможете локализовать и их.',
+    body_en:
+      'Different standards may need different levels of granularity in the date and time, so this profile defines six levels. Standards that reference this profile should specify one or more of these granularities. If a given standard allows more than one granularity, it should specify the meaning of the dates and times with reduced precision, for example, the result of comparing two dates with different precisions.'
+  },
+  {
+    id: 5,
+    title: 'Заголовок статьи',
+    title_en: 'Article title',
+    date: '1997-07-16T19:20+01:00',
+    body:
+      'Вы можете поместить файлы локализаций, совместимые с vue-i18n в каталог locales в корне вашего плагина. Они будут автоматически загружены в клиент при открытии проекта. Вы можете использовать $t для перевода строк в ваших компонентах и другие возможности vue-i18n. Также, любые строки используемые в UI API (такие как describeTask) будут также обрабатываться vue-i18n, поэтому вы сможете локализовать и их.',
+    body_en:
+      'Different standards may need different levels of granularity in the date and time, so this profile defines six levels. Standards that reference this profile should specify one or more of these granularities. If a given standard allows more than one granularity, it should specify the meaning of the dates and times with reduced precision, for example, the result of comparing two dates with different precisions.'
   }
 ];
